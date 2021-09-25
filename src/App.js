99f824bc496da7b149b11491c596c7c150c9ab82
rtl9148 <robertloza2021@u.northwestern.edu>
2021-09-25 17:06:21 -0500

    change app title and change page contents

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -21,5 +22,20 @@ function App() {
     </div>
   );
 }
+
+export default App;
+*/
+
+import React from 'react';
+
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
+
+const App = () =>  (
+  <div>
+    <h1>{ schedule.title }</h1>
+  </div>
+);
 
 export default App;
